76be267cfec4734c7ccd621b23302cb3aeec75b7
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-18 14:33:38 -0700

    commit tree 406008a8737f2a28e111c34da044aabc129d99d3

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+    def initialize atr
+        atr.each { |k, v| 
+            self.class.attr_accessor(k)
+            self.send("#{k}=", v)}
+    end
+
+    def save name, type, id
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, name, type)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
 end
